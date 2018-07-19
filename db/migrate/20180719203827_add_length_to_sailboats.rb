eaf2f9b9162135f1ac304e1f9bb3919ad3292c16
Blair <blairjacklane@gmail.com>
2018-07-19 18:20:06 -0400

    Adding

--- db/migrate/20180719203827_add_length_to_sailboats.rb
+++ db/migrate/20180719203827_add_length_to_sailboats.rb
@@ -1,4 +1,4 @@
-class AddLengthToSailboat < ActiveRecord::Migration[5.2]
+class AddLengthToSailboats < ActiveRecord::Migration[5.2]
   def change
     add_column :sailboats, :length, :integer
   end
