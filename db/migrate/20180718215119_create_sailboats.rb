adbbae410c561112b429071fa096e65659bc8eca
Blair <blairjacklane@gmail.com>
2018-07-19 20:08:40 -0400

    Cool

--- db/migrate/20180718215119_create_sailboats.rb
+++ db/migrate/20180718215119_create_sailboats.rb
@@ -7,7 +7,7 @@ class CreateSailboats < ActiveRecord::Migration[5.2]
       t.string :condition
       t.string :finish
       t.string :title
-      t.decimal :price, precision: 5, scale: 2, default: 0
+      t.decimal :price, scale: 2, default: 0
 
       t.timestamps
     end
